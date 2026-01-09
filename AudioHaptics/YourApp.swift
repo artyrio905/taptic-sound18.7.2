b995c9fcd2ede0b37208a5848bc8950b48fe73d5
artyrio905 <93124413+artyrio905@users.noreply.github.com>
2026-01-09 14:49:01 +0200

    Remove HapticPlayerViewModel from app structure
    Removed HapticPlayerViewModel from YourApp.

--- AudioHaptics/YourApp.swift
+++ AudioHaptics/YourApp.swift
@@ -2,15 +2,9 @@ import SwiftUI
 
 @main
 struct YourApp: App {
-    @StateObject private var vm = HapticPlayerViewModel()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(vm)
-                .onOpenURL { url in
-                    vm.handleIncomingURL(url)
-                }
         }
     }
 }
